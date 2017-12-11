64bd2c77d8c39ad1dd4290d7e0b319a7cb4c6827
Alec Lam <alechunterlam@gmail.com>
2017-12-11 13:32:46 -0500

    deploy please

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,6 @@ module SportsAPI
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.time_zone = 'Eastern (US & Canada)'
+    config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
